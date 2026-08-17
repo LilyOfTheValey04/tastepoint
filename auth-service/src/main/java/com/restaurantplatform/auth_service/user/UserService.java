1fc9286e33fd5487911736fa1bec383b138c606b
Lily <lilymaria044@gmail.com>
2026-08-17 14:54:15 +0300

    method get current user

--- auth-service/src/main/java/com/restaurantplatform/auth_service/user/UserService.java
+++ auth-service/src/main/java/com/restaurantplatform/auth_service/user/UserService.java
@@ -1,12 +1,26 @@
 package com.restaurantplatform.auth_service.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-   final UserRepository userRepository;
+   private final UserRepository userRepository;
+
+    public UserResponse getCurrentUser(Authentication authentication) {
+
+        User user = (User) authentication.getPrincipal();
+
+        return new UserResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getRole()
+        );
+    }
 
     public User getUserById(Long id) {
         return userRepository.findById(id)
